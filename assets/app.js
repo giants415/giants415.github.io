b90658a77c6116c424b140f7f36ca5ce7379420a
Andrew <andrew.vinocur@gmail.com>
2017-04-10 12:53:57 -0700

    in progress changes from last work

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,12 @@
 console.log('app.js connected');
+
+$document.ready(
+
+  $('#one').mouseover(function() {
+    $('#two').addClass('trigger');
+    setTimeout(function(){
+      $('#two').removeClass('trigger');
+    }, 10000);
+  });
+
+);
