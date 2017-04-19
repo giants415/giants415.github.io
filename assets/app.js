5a1c8e06d2e90dfdf40e36730a61065bc4587b7f
Andrew <andrew.vinocur@gmail.com>
2017-04-19 15:07:55 -0700

    ready to launch v1.0, all changes up to 4.14.2017

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,3 @@
-console.log('app.js connected');
-
-$document.ready(
-
-  $('#one').mouseover(function() {
-    $('#two').addClass('trigger');
-    setTimeout(function(){
-      $('#two').removeClass('trigger');
-    }, 10000);
-  });
-
-);
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
