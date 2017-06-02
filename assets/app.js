473cc0aee8db10047d9933052bc7c84cf698099e
Andrew <andrew.vinocur@gmail.com>
2017-06-02 14:50:05 -0700

    removes console log from when i was testing

--- assets/app.js
+++ assets/app.js
@@ -6,12 +6,10 @@ $(function () {
   });
 
   $(".port-item > img").on("mouseover", function(){
-    console.log('img hit');
     $(this).addClass("zoom");
   });
 
   $(".port-item > img").on("mouseout", function(){
-    console.log('img hit');
     $(this).removeClass("zoom");
   });
 
