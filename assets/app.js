f22197a6f6fa46b81110991405cf3e0d0e05e8d1
Andrew <andrew.vinocur@gmail.com>
2017-06-02 14:42:05 -0700

    creates zoom class which is applied or removed when mousing over port item

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,14 @@ $(function () {
     $('#resume-img').toggle();
   });
 
-  $("#portfolio img").on("click", function(){
-    $(this).css("zoom", "50%");
+  $(".port-item > img").on("mouseover", function(){
+    console.log('img hit');
+    $(this).addClass("zoom");
+  });
+
+  $(".port-item > img").on("mouseout", function(){
+    console.log('img hit');
+    $(this).removeClass("zoom");
   });
 
 });
