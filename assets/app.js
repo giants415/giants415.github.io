834585288f661bdf9d9eb9852dab25205e18d2c1
Andrew_Vinocur <andrew.vinocur@gmail.com>
2018-12-07 20:29:14 -0800

    scrolling nav in progress

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,16 @@
 function getScrollHeight() {
   var scrollHeight = $(window).scrollTop();
   console.log(scrollHeight);
+  if (scrollHeight >= 215) {
+    console.log('baboey');
+    $('nav').css({
+      'position' : 'fixed',
+      'top' : '0',
+      'padding-top' : '5vh',
+      'height' : '10vh',
+      'background' : '#FFFFFF'
+    });
+  }
 };
 
 $(document).ready(function(){
