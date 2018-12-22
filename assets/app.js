b68ac5abc4c5722979d0882e5d2a96b2a7acfb9a
Andrew_Vinocur <andrew.vinocur@gmail.com>
2018-12-22 14:02:57 -0800

    postponing dynamic navbar

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,25 @@
-function getScrollHeight() {
-  var scrollHeight = $(window).scrollTop();
-  console.log(scrollHeight);
-  if (scrollHeight >= 215) {
-    console.log('baboey');
-    $('nav').css({
-      'position' : 'fixed',
-      'top' : '0',
-      'padding-top' : '5vh',
-      'height' : '10vh',
-      'background' : '#FFFFFF'
-    });
-  }
-};
+// function getScrollHeight() {
+//   var scrollHeight = $(window).scrollTop();
+//   console.log(scrollHeight);
+//   while (scrollHeight <= 270) {
+//   var $navHeight = $('nav').height();
+//   console.log($navHeight);
+//     $('nav').height()
+//   }
+//   if (scrollHeight >= 215) {
+//     console.log('baboey');
+//     $('nav').css({
+//       'position' : 'fixed',
+//       'top' : '0',
+//       'padding-top' : '5vh',
+//       'height' : '10vh',
+//       'background' : '#FFFFFF'
+//     });
+//   }
+// };
 
 // working to get nav bar to stay a fixed height after scroll height reaches a boundry
 
 $(document).ready(function(){
-  $(document).scroll(getScrollHeight);
+  // $(document).scroll(getScrollHeight);
 });
